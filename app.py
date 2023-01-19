112007cf2e74df5231e918db1f796c136b694cf1
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-01-19 16:28:01 +0100

    modify loop

--- app.py
+++ app.py
@@ -5,21 +5,23 @@ import pyautogui
 SCREEN_SIZE = tuple(pyautogui.size())
 
 fourcc = cv2.VideoWriter_fourcc(*"XVID")
-
 fps = 12.0
-
 video_output = cv2.VideoWriter("output.avi", fourcc, fps, SCREEN_SIZE)
-
 record_seconds = 10
 
+Xs = [0,8,6,14,12,4,2,0]
+Ys = [0,2,4,12,14,6,8,0]
 
-for i in range(int(record_seconds * fps)):    
+# for i in range(int(record_seconds * fps)):   
+while True: 
     img = pyautogui.screenshot()
     frame = np.array(img)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
     video_output.write(frame)
-    # cv2.imshow("screenshot", frame)
-    if cv2.waitKey(1) == ord("q"):
+    image = cv2.resize(frame, (960, 540))   
+    cv2.imshow("screenshot", image)
+    if cv2.waitKey(1) == 27:
         break
 
 cv2.destroyAllWindows()
